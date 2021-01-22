4b66f8cb5ebdcc9e4e22b9ea6b7151e0f28a2b08
hollyjphilly <61254925+hollyjphilly@users.noreply.github.com>
2021-01-22 17:24:07 -0500

    Worked on second_anagram

--- anagrams.rb
+++ anagrams.rb
@@ -9,8 +9,11 @@ p first_anagram?("elvis", "lives")    #=> true
 
 def second_anagram?(str1, str2)
     str1.chars.each_with_index do |char, i|
-        str2.find_index(char) == i
-
+        str2.delete(char) if str2.chars.find_index(char) == i 
+        #stopped about to fix ^^ this line to delete at the index we find in the conditional instead of at the char      
     end
-
+    str2.empty?
 end
+
+p second_anagram?("gizmo", "sally")    #=> false
+p second_anagram?("elvis", "lives")    #=> true
